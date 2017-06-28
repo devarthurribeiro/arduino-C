5bad159659b7e73df82d905ba259a5cd97dfe287
Arthur Ribeiro <devarthurribeiro@gmail.com>
2017-06-28 00:58:37 -0300

    fix serial

--- arduino.cpp
+++ arduino.cpp
@@ -6,35 +6,11 @@ void setup() {
   pinMode(trava, OUTPUT);
   digitalWrite(trava, LOW);
 }
-
-char lerStringSerial(){
-  String conteudo = "";
-  char caractere;
-
-  // Enquanto receber algo pela serial
-  while(Serial.available() > 0) {
-    // Lê byte da serial
-    caractere = Serial.read();
-    // Ignora caractere de quebra de linha
-    if (caractere != '\n'){
-      // Concatena valores
-      conteudo.concat(caractere);
-    }
-    // Aguarda buffer serial ler próximo caractere
-    delay(10);
-  }
-
-  Serial.print("Recebi: ");
-  Serial.println(conteudo);
-
-  return conteudo;
-}
-
 void loop() {
   char dado;
   int aux;
-  if(Serial.available() > 0){
-    dado = lerStringSerial();
+  if(Serial.available()){
+    dado = Serial.read();
     if (dado == 'a'){
       Serial.println("Comando recebido");
       digitalWrite(trava, HIGH);
